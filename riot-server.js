a0e6ef837cbdbade4622afe7249a90621fdab5b7
Roman Akhmadullin <freeman.aka.ahmed@gmail.com>
2015-09-09 07:40:03 +0500

    Fix working with 'eval' function

--- riot-server.js
+++ riot-server.js
@@ -1,7 +1,7 @@
 var riot = require('riot'), fs = require('fs');
 
 var example = riot.compile(fs.readFileSync('./example.tag-jade', 'utf8'), { template: 'jade' });
+eval(example);
 
 console.log(riot.render('example'));
-console.log(riot.render(example));
 
